bdb8066c3ee886e7fe5a64d6501012c30779f527
18kingj <161600931+18kingj@users.noreply.github.com>
2024-03-07 10:59:34 +0000

    1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,20 +1,11 @@
 # Your solution to Exercise 1
 
-age = input()
-if age[1] == ' ':
-    a = age[0]
+age_alex = int(input("Enter Alex's age: "))
+age_tatyana = int(input("Enter Tatyana's age: "))
+
+if age_alex > age_tatyana:
+    print("Alex is the eldest.")
+elif age_alex < age_tatyana:
+    print("Tatyana is the eldest.")
 else:
-    a = age[0] + age[1]
-if len(age) == 4:
-    if len(a) == 2:
-        print('Alex is the eldest.')
-    else:
-        print('Tatyana is the eldest.')
-else:
-    t = age[3] + age[4]
-    if int(a) > int(t):
-        print('Alex is the eldest.')
-    elif int(a) < int(t):
-        print('Tatyana is the eldest.')
-    else:
-        print('Alex and Tatyana are of the same age.')
+    print("Alex and Tatyana are of the same age.")
